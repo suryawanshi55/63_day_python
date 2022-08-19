b751caa09cc714249efd70eef675d3e35614d6ae
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-19 14:46:15 +0530

    Add files via upload

---
+++ signup_login.py
@@ -0,0 +1,9 @@
+# usr=input('Enter Username: ')
+# pwd=input('Enter Password: ')
+# print('Account Created')
+# name=input('Enter Username: ')
+# psw=input('Enter Password: ')            # Enter Username: pooja
+# if(usr==name and psw==pwd):                # Enter Password: 7890
+#     print('you have logged in succesfully')        #Account Created
+# else:
+#     print('Invalid Details')
